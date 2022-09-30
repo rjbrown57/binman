6f256d5580001152eb2581da2bbbc9ca3b397f40
Aaron U'Ren <aauren@users.noreply.github.com>
2022-09-30 16:56:29 -0500

    fix(types): change sprintf to filepath.Join

--- pkg/types.go
+++ pkg/types.go
@@ -1,7 +1,6 @@
 package binman
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -67,7 +66,7 @@ func (r *BinmanRelease) setArtifactPath(ReleasePath string, tag string) {
 	if strings.HasSuffix(ReleasePath, "/") {
 		ReleasePath = strings.TrimSuffix(ReleasePath, "/")
 	}
-	r.PublishPath = fmt.Sprintf("%s/repos/%s/%s/%s", ReleasePath, r.Org, r.Project, tag)
+	r.PublishPath = filepath.Join(ReleasePath, "repos", r.Org, r.Project, tag)
 }
 
 // Helper method to set paths for a requested release object
@@ -87,26 +86,26 @@ func (r *BinmanRelease) setPublishPaths(ReleasePath string, assetName string) {
 	// else if it's a tar but we have specified the inside file use filename for source and destination
 	// else we want default
 	if r.ReleaseFileName != "" {
-		r.ArtifactPath = fmt.Sprintf("%s/%s", r.PublishPath, r.ReleaseFileName)
+		r.ArtifactPath = filepath.Join(r.PublishPath, r.ReleaseFileName)
 		log.Debugf("ReleaseFilenName set %s\n", r.ArtifactPath)
 	} else if r.ExtractFileName != "" {
-		r.ArtifactPath = fmt.Sprintf("%s/%s", r.PublishPath, r.ExtractFileName)
+		r.ArtifactPath = filepath.Join(r.PublishPath, r.ExtractFileName)
 		log.Debugf("Tar with Filename set %s\n", r.ArtifactPath)
 	} else if r.ExternalUrl != "" {
-		r.ArtifactPath = fmt.Sprintf("%s/%s", r.PublishPath, filepath.Base(r.ExternalUrl))
+		r.ArtifactPath = filepath.Join(r.PublishPath, filepath.Base(r.ExternalUrl))
 		log.Debugf("Tar with Filename set %s\n", r.ArtifactPath)
 	} else {
 		// If we find a tar in the assetName assume the name of the binary within the tar
 		// Else our default is a binary
 		if isTar(assetName) {
-			r.ArtifactPath = fmt.Sprintf("%s/%s", r.PublishPath, r.Project)
+			r.ArtifactPath = filepath.Join(r.PublishPath, r.Project)
 		} else {
-			r.ArtifactPath = fmt.Sprintf("%s/%s", r.PublishPath, assetName)
+			r.ArtifactPath = filepath.Join(r.PublishPath, assetName)
 		}
 		log.Debugf("Default Extraction %s\n", r.ArtifactPath)
 	}
 
-	r.LinkPath = fmt.Sprintf("%s/%s", ReleasePath, linkName)
+	r.LinkPath = filepath.Join(ReleasePath, linkName)
 	log.Debugf("Artifact Path %s Link Path %s\n", r.ArtifactPath, r.Project)
 
 }
