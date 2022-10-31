67759cb7feb43b0fe7042395d46860d0ce72c507
Russell Brown <rjbrown57@gmail.com>
2022-10-30 23:12:12 -0500

    test: add TestSetDefaults

--- pkg/types_test.go
+++ pkg/types_test.go
@@ -32,6 +32,28 @@ releases:
   - repo: rjbrown57/binman
 `
 
+const testConfig = `
+config:
+  releasepath: thereleasepath
+  tokenvar: thetoken
+  upx:
+    enabled: true
+    args: []
+releases:
+  - repo: rjbrown57/binman
+`
+
+const testConfigEmptyVals = `
+config:
+  releasepath: 
+  tokenvar:
+  upx:
+    enabled: true
+    args: []
+releases:
+  - repo: rjbrown57/binman
+`
+
 func TestDeduplicate(t *testing.T) {
 
 	d, err := os.MkdirTemp(os.TempDir(), "binmantest")
@@ -53,5 +75,54 @@ func TestDeduplicate(t *testing.T) {
 
 	if len(c.Releases) != 2 {
 		t.Fatal("failed to dedeuplicate release array")
+	}
+}
+
+func TestSetDefaults(t *testing.T) {
+	d, err := os.MkdirTemp(os.TempDir(), "binmantest")
+	if err != nil {
+		t.Fatalf("unable to make temp dir %s", d)
+	}
+
+	defer os.RemoveAll(d)
+
+	configPath := fmt.Sprintf("%s/config", d)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatalf("Unable to detected user home directory %s", err)
+	}
+
+	var tests = []struct {
+		config              string
+		expectedOs          string
+		expectedArch        string
+		expectedReleasePath string
+		expectedTokenVar    string
+	}{
+		{testConfig, "linux", "amd64", "thereleasepath", "thetoken"},
+		{testConfigEmptyVals, "linux", "amd64", homeDir + "/" + "binMan", "none"},
+	}
+
+	for _, test := range tests {
+		writeStringtoFile(configPath, test.config)
+		if err != nil {
+			t.Fatalf("failed to write test config to %s", configPath)
+		}
+		c := newGHBMConfig(configPath)
+		c.setDefaults()
+
+		// test the defaults
+		if c.Defaults.Arch != test.expectedArch || c.Defaults.Os != test.expectedOs {
+			t.Fatalf("Expected %s,%s got %s,%s", c.Defaults.Arch, c.Defaults.Os, test.expectedArch, test.expectedOs)
+		}
+
+		if c.Config.TokenVar != test.expectedTokenVar {
+			t.Fatalf("Expected %s got %s", test.expectedTokenVar, c.Config.TokenVar)
+		}
+
+		if c.Config.ReleasePath != test.expectedReleasePath {
+			t.Fatalf("Expected %s got %s", test.expectedReleasePath, c.Config.ReleasePath)
+		}
 	}
 }
