583ad7a865a78fb6234a4a349a22db6cb6815150
Russell Brown <rjbrown57@gmail.com>
2026-03-05 16:02:12 -0500

    refactor: simplify gh limits test

--- pkg/gh/limits_test.go
+++ pkg/gh/limits_test.go
@@ -1,22 +1,18 @@
 package gh
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/google/go-github/v50/github"
 )
 
 func TestGetLimits(t *testing.T) {
 
 	client := GetGHCLient(defaultGHBaseURL, "none")
-	_, err := getLimits(client)
+	limits, err := getLimits(client)
 	if err != nil {
 		t.Errorf("unable to get limits, %s", err)
 	}
 
-	// Based on https://github.com/google/go-github/blob/master/github/github_test.go#L468
-	if got, want := reflect.TypeFor[github.RateLimits]().NumField(), reflect.TypeFor[github.RateLimits]().NumField(); got != want {
-		t.Errorf("len(Client{}.rateLimits) is %v, want %v", got, want)
+	if limits.Core.Limit != 60 {
+		t.Fatal("Failed to get basic limits")
 	}
 }
